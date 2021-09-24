940240bb951c1eb814f34180d34a028ac6f1a974
Jonagarme <garciajonathan268@gmail.com>
2021-09-24 10:15:58 -0500

    dimensiones, boxObjectModel

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {SafeAreaView} from 'react-native';
-import {BoxObjectModelScreen} from './src/screens/BoxObjectModelScreen';
+//import {BoxObjectModelScreen} from './src/screens/BoxObjectModelScreen';
+import {DimensionesScreen} from './src/screens/DimensionesScreen';
 //import {ContadorScree} from './src/screens/ContadorScree';
 //import {HolaMundoScreen} from './src/screens/HolaMundoScreen';
 
 export const App = () => {
   return (
-    <SafeAreaView>
-      <BoxObjectModelScreen />
+    <SafeAreaView style={{flex: 1}}>
+      {/*  <BoxObjectModelScreen /> */}
+      <DimensionesScreen />
     </SafeAreaView>
   );
 };
--- src/screens/BoxObjectModelScreen.tsx
+++ src/screens/BoxObjectModelScreen.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 
 export const BoxObjectModelScreen = () => {
   return (
-    <View>
-      <Text>BoxObject model</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>BoxObject model</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  title: {
+    padding: 50,
+    fontSize: 20,
+    //width: 150,
+    borderWidth: 10,
+    //backgroundColor: 'red',
+  },
+});
